f552732345fef228ca15f0112babb09e3795eee1
Daniel Jr <lopezdanieljr10@gmail.com>
2025-06-25 18:42:17 -0500

    Se arreglo menu lateral  y responsividad

--- pantallaprincipal/principal.js
+++ pantallaprincipal/principal.js
@@ -1,17 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const botonAgregar = document.getElementById('botonAgregar');
-    const menuOpciones = document.getElementById('menuOpciones');
-  
-    botonAgregar.addEventListener('click', function (e) {
-      e.preventDefault();
-      menuOpciones.classList.toggle('d-none');
-    });
-  
-    // (Opcional) Cerrar menú si haces clic fuera
-    document.addEventListener('click', function (event) {
-      if (!menuOpciones.contains(event.target) && !botonAgregar.contains(event.target)) {
-        menuOpciones.classList.add('d-none');
-      }
-    });
+  const botonAgregar = document.getElementById('botonAgregar');
+  const menuOpciones = document.getElementById('menuOpciones');
+
+  botonAgregar.addEventListener('click', function (e) {
+    e.preventDefault();
+    menuOpciones.classList.toggle('d-none');
   });
-  
+
+  // Cierra el menú si haces clic fuera
+  document.addEventListener('click', function (event) {
+    if (!menuOpciones.contains(event.target) && !botonAgregar.contains(event.target)) {
+      menuOpciones.classList.add('d-none');
+    }
+  });
+});
